01087c31191088f2f04cba80ff3171cab52384d6
FedericoCalzoni <56981117+FedericoCalzoni@users.noreply.github.com>
2024-09-16 01:42:15 +0200

    Update PDF-Margin.sh
    Add: Possibility to handle multiple files, better output handling

--- PDF-Margin.sh
+++ PDF-Margin.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-# Check if an input PDF file is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <input.pdf>"
+# Check if at least one input PDF file is provided as an argument
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <input1.pdf> [input2.pdf] ..."
     exit 1
 fi
 
-input_pdf="$1"
+# Iterate over each input PDF file
+for input_pdf in "$@"; do
+    if [ ! -f "$input_pdf" ]; then
+        echo "Error: The input PDF file '$input_pdf' does not exist."
+        continue
+    fi
 
-# Check if the input PDF file exists
-if [ ! -f "$input_pdf" ]; then
-    echo "Error: The input PDF file '$input_pdf' does not exist."
-    exit 1
-fi
+    # Extract the base name of the input file (without the extension)
+    inputname=$(basename "$input_pdf" .pdf)
 
-# Extract the base name of the input file (without the extension)
-inputname=$(basename "$input_pdf" .pdf)
+    # Check if the output PDF file already exists
+    if [ -f "${inputname}-wide.pdf" ]; then
+        echo "Error: The output PDF file '${inputname}-wide.pdf' already exists."
+        continue
+    fi
 
-# Check if the output PDF file already exists
-if [ -f "${inputname}-wide.pdf" ]; then
-    echo "Error: The output PDF file '${inputname}-wide.pdf' already exists."
-    exit 1
-fi
+    # Use pdfjam to resize and translate the content of the PDF
+    pdfjam --outfile "${inputname}-wide.pdf" --papersize '{367mm,210mm}' --scale '0.95' --offset '-40mm 0' "$input_pdf" > /dev/null 2>&1
 
-# Use pdfjam to resize and translate the content of the PDF
-pdfjam --outfile "${inputname}-wide.pdf" --papersize '{367mm,210mm}' --scale '0.95' --offset '-40mm 0' "$input_pdf"
-
-echo "Output PDF: ${inputname}-wide.pdf"
-
-
-
+    echo "Output PDF: ${inputname}-wide.pdf"
+done
